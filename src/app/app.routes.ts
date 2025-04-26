fae9ad5451042061ff18afe46f70b7f80e50619e
Don Lasagne <184601225+Don-Lasagne@users.noreply.github.com>
2025-04-26 18:56:29 +0200

    update favicon, add new components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
+import {UebersichtComponent} from './uebersicht/uebersicht.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: "start", component: UebersichtComponent, pathMatch: "full"},
+  {path: "**", redirectTo: "start"}
+];
